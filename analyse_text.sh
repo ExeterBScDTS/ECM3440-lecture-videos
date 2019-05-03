e5d3ab053e07292ce1d0b69d0ad839fc1a1edeac
Michael Saunby <mike@saunby.net>
2019-05-03 18:54:23 +0100

    .

--- analyse_text.sh
+++ analyse_text.sh
@@ -4,5 +4,5 @@ export GOOGLE_APPLICATION_CREDENTIALS="/home/mike/Downloads/IoC-Summer-School-25
 
 curl -s -H "Content-Type: application/json" \
     -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
-    https://language.googleapis.com/v1beta2/documents:analyzeSyntax \
+    https://language.googleapis.com/v1/documents:analyzeSyntax \
     -d @script-type-hints.json
