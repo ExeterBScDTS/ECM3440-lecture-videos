10f794d52bc12046415e2bd219dcd165e1eb9d75
Michael Saunby <mike@saunby.net>
2019-05-04 14:18:21 +0100

    Delete extract_audio.sh

--- extract_audio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-infile="videos/ECM3440 - Week 2 - Lesson 1a - Python type hints - Slides.mp4"
-outfile="audio/type-hints.mp3"
-ls -l "${infile}"
-ffmpeg -i "${infile}" -f mp3 -ab 44000 -vn ${outfile}
